9e7375a9ebdf17401b33cd1fd87818eb112a0bc6
nurbeyzanur <117708453+nurbeyzanur@users.noreply.github.com>
2022-11-30 14:52:37 +0300

    Add files via upload
    Klavyeden girilen iki tam sayının dost sayı olup olmadığını bulan program.

---
+++ proje27.java
@@ -0,0 +1,34 @@
+package projeler;
+//m 
+
+import java.util.Scanner;
+
+public class proje27 {
+    public static void main(String[] args) {
+        Scanner scan= new Scanner(System.in);
+        int a,b;
+        int tpa=0,tpb=0;
+        System.out.println("Lütfen ilk sayıyı giriniz:");
+        a = scan.nextInt();
+        
+        System.out.println("Lütfen ikinci sayıyı giriniz:");
+        b = scan.nextInt();
+        
+        for(int i=1;i<=a;i++){
+            if (a%i==0) {
+               tpa=tpa+i;                
+            }            
+        }
+        for(int j=1;j<=b;j++){
+            if (b%j==0) {
+               tpb=tpb+j;                
+            }            
+        }
+        if (tpa==tpb){
+            System.out.println(a+" ve " +b+" dost sayılardır.");            
+        }
+        else{
+            System.out.println("Girdiğiniz sayılar dost sayı değildir.");
+        }
+    }  
+}
